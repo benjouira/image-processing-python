e139fe35a057e7ad1c5fec6d5f1b63d91b5dadb4
Rabii <rabi3jouira@gmail.com>
2021-11-10 22:27:17 +0100

    Create imgProc.py

---
+++ imgProc.py
@@ -0,0 +1,29 @@
+import numpy as np
+#Importation du module image
+import matplotlib.image as mpimg
+#Importation du module pyplot
+import matplotlib.pyplot as plt
+#Lecture d'une image au format png dans Python
+img = mpimg.imread("Joconde.png")
+#Affichage
+plt.imshow(img)
+#Sauvegarde sur disque
+# plt.imsave ("Joconde.png ", image)
+
+# liste de pixels
+print(img*255)
+
+# global size 
+print(img.size)
+
+# long 
+print(img.shape[0])
+
+# larg
+print(img.shape[1])
+
+# Taille
+print(img.shape[0]*img.shape[1])
+
+# Dimensions
+print(img.shape)
